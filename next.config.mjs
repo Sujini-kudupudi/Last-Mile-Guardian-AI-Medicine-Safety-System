f6ba5fa11a9b6f1a4f407a404e94784e32fc0423
sujini kudipudi <sujini.kudipudi@gmail.com>
2026-05-03 13:17:55 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",   // 🔥 REQUIRED for GitHub Pages
+
+  basePath: "/Last-Mile-Guardian-AI-Medicine-Safety-System",
+  assetPrefix: "/Last-Mile-Guardian-AI-Medicine-Safety-System/",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
